cc55667d4d43c2bb6ea44a659489e12ba171e681
Viktor Balonkin <v.balonkin@gmail.com>
2025-05-24 00:51:37 +0300

    validate_jwt

---
+++ app/services/auth.py
@@ -0,0 +1,23 @@
+import httpx
+
+from app.services.cache import cache_jwt, get_cached_jwt
+from config import settings
+
+
+async def validate_jwt(token: str) -> dict | None:
+    # Проверка кэша
+    if cached := get_cached_jwt(token):
+        return cached
+
+    # Запрос к auth-service
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{settings.auth_service_url}/validate", json={"token": token}
+        )
+
+    if response.status_code == 200:
+        data = response.json()
+        await cache_jwt(token, data)
+        return data
+
+    return None
