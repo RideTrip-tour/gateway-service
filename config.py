571ea815ed8078c27952e3b793ed10d2dc2b967f
Viktor Balonkin <v.balonkin@gmail.com>
2026-01-26 22:20:20 +0300

    docker

--- config.py
+++ config.py
@@ -1,34 +1,36 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    redis_url: str = "redis://127.0.0.1:6379"
+    jwt_secret: str = os.getenv("JWT_SECRET", "secret")
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM", "HS256")
+    
+    # Redis
+    redis_url: str = os.getenv("REDIS_URL", "redis://redis:6379")
     redis_ttl: int = 300  # время жизни кеша по умолчанию
     rate_limit: int = 100  # запросов в минуту
     public_paths: list[str] = [
         "/health",
-        "/auth/login",
-        "/auth/register",
+        "/api/auth/login",
+        "/api/auth/register",
+        "/api/auth/docs",
         "/docs",
         "/openapi.json",
         "/redoc",
     ]
     service_map: dict[str, str] = {
-        "auth": "http://127.0.0.1:8001/api",
-        "plans": "http://plans-service:8001",
-        "locations": "http://locations-service:8002",
-        "users": "http://users-service:8003",
-        "activities": "http://activities-service:8004",
-        "routes": "http://routes-service:8005",
-        "departure": "http://departure-service:8006",
-        "pricing": "http://pricing-service:8007",
-        "pdf": "http://pdf-service:8008",
-        "bot": "http://bot-service:8009",
+        "auth": "http://auth-service:8000",
+        "plans": "http://plans-service:8000",
+        "locations": "http://locations-service:8000",
+        "users": "http://users-service:8000",
+        "activities": "http://activities-service:8000",
+        "routes": "http://routes-service:8000",
+        "departure": "http://departure-service:8000",
+        "pricing": "http://pricing-service:8000",
+        "pdf": "http://pdf-service:8000",
+        "bot": "http://bot-service:8000",
     }
-
-    model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", env_ignore_empty=True
-    )
 
 
 settings = Settings()
